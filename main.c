cad28a6ec0a6ce440b1c06653a57b72d576e27e2
TheArielX <arieltiburcio@gmail.com>
2016-06-22 17:20:11 -0400

    Update main.c
    Ojo, programa ya listo para correr.
    0 bugs hasta el momento.

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-//hay problemas
-int cdnp(int a, int b, int c, int cc, int d, int e, int x){
-    if(a>=0 && a<=9) {
+int cdnp(int a, int b, int bb, int c, int cc, int d, int dd, int e, int x){
+    if(a>=0 && a<=9 || a<=0 && a>=-9) {
         if (a%2==0){
             printf("El n%cmero %d es par\n", 163, a);
             x++;
@@ -10,9 +9,14 @@ int cdnp(int a, int b, int c, int cc, int d, int e, int x){
         else {
             printf("El n%cmero %d es impar\n", 163, a);
         }
+        if(x==1){
+            printf("El n%cmero tiene %d n%cmero par\n", 163, x, 163);
+        }
+        else{
         printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+        }
     }
-    if (a>=10 || a<=99) {
+    if (a>=10 && a<=99 || a<=-10 && a>=-99) {
         b=a/10;
         if (b%2==0){
             printf("El n%cmero %d es par\n", 163, b);
@@ -29,11 +33,15 @@ int cdnp(int a, int b, int c, int cc, int d, int e, int x){
         else {
             printf("El n%cmero %d es impar\n", 163, a);
     }
-    printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+    if(x==1){
+            printf("El n%cmero tiene %d n%cmero par\n", 163, x, 163);
+        }
+        else{
+        printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+        }
     }
-    if(a>=100 && x<=999)    {
-        cc=a/100;
-        c=cc/10;
+    if(a>=100 && a<=999 || a<=-100 && a>=-999)    {
+        c=a/100;
         if (c%2==0){
             printf("El n%cmero %d es par\n", 163, c);
             x++;
@@ -41,7 +49,8 @@ int cdnp(int a, int b, int c, int cc, int d, int e, int x){
         else {
             printf("El n%cmero %d es impar\n", 163, c);
         }
-        b=a/10;
+        bb=a%100;
+        b=bb/10;
         if (b%2==0){
             printf("El n%cmero %d es par\n", 163, b);
             x++;
@@ -57,12 +66,117 @@ int cdnp(int a, int b, int c, int cc, int d, int e, int x){
         else {
             printf("El n%cmero %d es impar\n", 163, a);
         }
+        if(x==1){
+            printf("El n%cmero tiene %d n%cmero par\n", 163, x, 163);
+        }
+        else{
         printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+        }
+    }
+    if(a>=1000 && a<=9999 || a<=-1000 && a>=-9999)    {
+        d=a/1000;
+        if (d%2==0){
+            printf("El n%cmero %d es par\n", 163, d);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, d);
+        }
+        cc=a/100;
+        c=cc%10;
+        if (c%2==0){
+            printf("El n%cmero %d es par\n", 163, c);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, c);
+        }
+        bb=a%100;
+        b=bb/10;
+        if (b%2==0){
+            printf("El n%cmero %d es par\n", 163, b);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, b);
+        }
+        a=a%10;
+        if (a%2==0){
+            printf("El n%cmero %d es par\n", 163, a);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, a);
+        }
+        if(x==1){
+            printf("El n%cmero tiene %d n%cmero par\n", 163, x, 163);
+        }
+        else{
+        printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+        }
+    }
+    if(a>=10000 && a<=99999 || a<=-10000 && a>=-99999)    {
+        e=a/10000;
+        if(e%2==0){
+            printf("El n%cmero %d es par\n", 163, e);
+            x++;
+        }
+        else{
+            printf("El n%cmero %d es impar\n", 163, e);
+        }
+        dd=a/1000;
+        d=dd%10;
+        if (d%2==0){
+            printf("El n%cmero %d es par\n", 163, d);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, d);
+        }
+        cc=a/100;
+        c=cc%10;
+        if (c%2==0){
+            printf("El n%cmero %d es par\n", 163, c);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, c);
+        }
+        bb=a%100;
+        b=bb/10;
+        if (b%2==0){
+            printf("El n%cmero %d es par\n", 163, b);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, b);
+        }
+        a=a%10;
+        if (a%2==0){
+            printf("El n%cmero %d es par\n", 163, a);
+            x++;
+        }
+        else {
+            printf("El n%cmero %d es impar\n", 163, a);
+        }
+        if(x==1){
+            printf("El n%cmero tiene %d n%cmero par\n", 163, x, 163);
+        }
+        else{
+        printf("El n%cmero tiene %d n%cmeros pares\n", 163, x, 163);
+        }
     }
 }
-int a, b, c, cc, d, e, x;
+int a, b, bb, c, cc, d, dd, e, x;
 int main()
 {
-    scanf("%d", &a);
-    cdnp(a, b, c, cc, d, e, x);
+    printf("Introduzca el n%cmero que desea consultar su cantidad de n%cmeros pares\nOjo: El n%cmero introducido no puede tener m%cs de 5 d%cgitos.\nIntroduzca el n%cmero: ", 163, 163, 163, 160, 161, 163);
+        scanf("%d", &a);
+            while (a>99999 || a<-99999) {
+                printf("El n%cmero %d tiene m%cs de 5 d%cgitos\nPor favor, introduzca un n%cmero con 5 d%cgitos o menos\n\n", 163, a, 160, 161, 163, 161);
+                    printf("Introduzca el n%cmero que desea consultar su cantidad de n%cmeros pares\nOjo: El n%cmero introducido no puede tener m%cs de 5 d%cgitos.\nIntroduzca el n%cmero: ", 163, 163, 163, 160, 161, 163);
+                        scanf("%d", &a);
+            }
+
+            cdnp(a, b, bb, c, cc, d, dd, e, x);
 }
